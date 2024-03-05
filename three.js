9f1e9e28051b5791e482ec0fb7023ae912c1ed1e
Alimkhan <magomadovalimhan@mail.ru>
2024-03-05 15:32:56 +0300

    First task

---
+++ three.js
@@ -0,0 +1,18 @@
+let person = {
+  name: "John",
+  age: 25,
+  job: "developer"
+}
+
+console.log(person);
+// Создайте объект person с полями name, age и job. Присвойте полям значения, например, "John", 25 и "developer" соответственно.
+
+let car = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2020,
+  color: "yelow"
+}
+
+console.log(car);
+// Создайте объект car с полями brand, model, year и color. Присвойте полям значения, например, "Toyota", "Camry", 2020 и "blue" соответственно.
